54af51ee28c2aa136114ef5aa6a97e9d9d8a7bdb
Ismail Mohamed <112773490+xrazerz@users.noreply.github.com>
2023-12-18 18:40:04 +0200

    Create search-in-rotated-sorted-array-ii.js
    LeetCode

---
+++ search-in-rotated-sorted-array-ii.js
@@ -0,0 +1,31 @@
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            return true;
+        }
+
+        // Handle duplicates
+        while (left < mid && nums[left] === nums[mid]) {
+            left++;
+        }
+
+        if (nums[left] <= nums[mid]) {
+            if (nums[left] <= target && target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } else {
+            if (nums[mid] < target && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+
+    return false;
